e429fc16b13c8472aa3f33df554e1e391b79cac1
Roméo Vandoolaeghe <90157318+RomeoVandoolaeghe@users.noreply.github.com>
2023-05-04 16:46:02 +0200

    Add files via upload

--- meteo.js
+++ meteo.js
@@ -2,27 +2,34 @@ function kelvin_to_celsius(temp) {
   return (temp = temp - 273.1).toFixed(2) + "°C";
 }
 
-const key="5ea75dbd783a69124434e60d6e087ec4";
-let city = "Lille"
+const key = "5ea75dbd783a69124434e60d6e087ec4";
+let city = "Paris";
 
-
+let hour=0;
 
 function request() {
-    console.log(city);
-
+  console.log(city);
+  hour++;
+  console.log(hour);
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
       city +
-      "&appid="+key+""
+      "&appid=" +
+      key +
+      ""
   )
     .then((response) => response.json())
     .then((response2) => {
+      console.log(response2.list[hour].dt_txt);
+      document.querySelector("h2").innerHTML=response2.list[hour].dt_txt;
+
+      console.log(response2.list[hour].main.temp);
       document.querySelector("h1").innerHTML = kelvin_to_celsius(
-        response2.main.temp
+        response2.list[hour].main.temp
       ); //température
 
       switch (
-        response2.weather[0].main //plusieurs cas du temps
+        response2.list[hour].weather[0].main //plusieurs cas du temps
       ) {
         case "Clouds":
           document.querySelector("img").src = "nuages.png";
@@ -34,4 +41,4 @@ function request() {
     });
 }
 
-setInterval(request, 1000); //boucle
+setInterval(request, 2000); //boucle
